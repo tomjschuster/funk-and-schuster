f2808856fb52e1a9bedab3e8481ea490816d2cdd
Tom Schuster <tomjschuster@gmail.com>
2018-07-06 16:31:05 -0400

    home hero paint blob picture

--- lib/funk_and_schuster_web/views/layout_view.ex
+++ lib/funk_and_schuster_web/views/layout_view.ex
@@ -1,3 +1,14 @@
 defmodule FunkAndSchusterWeb.LayoutView do
   use FunkAndSchusterWeb, :view
+
+  def page_class(conn) do
+    case conn.path_info do
+      [] -> "home"
+      ["about" | _rest] -> "about"
+      ["gallery" | _rest] -> "gallery"
+      ["artists" | _rest] -> "artists"
+      ["contact" | _rest] -> "contact"
+      _ -> ""
+    end
+  end
 end

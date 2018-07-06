2a6da11d2b10b3a228d290b566c0104f8b45c7bd
Tom Schuster <tomjschuster@gmail.com>
2018-07-06 00:35:54 -0400

    add moquin theme as static asset

--- lib/funk_and_schuster_web/endpoint.ex
+++ lib/funk_and_schuster_web/endpoint.ex
@@ -9,7 +9,7 @@ defmodule FunkAndSchusterWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :funk_and_schuster, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(moquin css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.

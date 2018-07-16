ea271e5ff887f605900f9cc954909b1b7fe5ac74
Tom Schuster <tomjschuster@gmail.com>
2018-07-15 23:58:46 -0400

    add sass dep

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -20,7 +20,10 @@ exports.config = {
       // }
     },
     stylesheets: {
-      joinTo: 'css/app.css'
+      joinTo: 'css/app.css',
+      order: {
+        after: ['priv/static/css/app.scss'] // concat app.css last
+      }
     },
     templates: {
       joinTo: 'js/app.js'

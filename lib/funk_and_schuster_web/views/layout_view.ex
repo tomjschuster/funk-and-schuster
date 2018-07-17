85bc657077ed291d91dc7d2248747b4d97b3381c
Tom Schuster <tomjschuster@gmail.com>
2018-07-17 00:42:00 -0400

    active link is bold

--- lib/funk_and_schuster_web/views/layout_view.ex
+++ lib/funk_and_schuster_web/views/layout_view.ex
@@ -1,3 +1,9 @@
 defmodule FunkAndSchusterWeb.LayoutView do
   use FunkAndSchusterWeb, :view
+
+  def nav_link(conn, page, label),
+    do: link(label, to: page_path(conn, page), class: active_class(conn, page))
+
+  def active_class(conn, page),
+    do: if(page_path(conn, page) == conn.request_path, do: "active", else: "")
 end

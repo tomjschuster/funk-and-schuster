a4d712846a2920f4e506659bc73f8ada7caecffb
Tom Schuster <tomjschuster@gmail.com>
2018-07-17 02:26:01 -0400

    wokring on deployment

--- mix.exs
+++ mix.exs
@@ -41,7 +41,9 @@ defmodule FunkAndSchuster.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:thesis, "~> 0.3.0"}
+      {:thesis, "~> 0.3.0"},
+      {:edeliver, "~> 1.4.3"},
+      {:distillery, "~> 1.4"}
     ]
   end
 

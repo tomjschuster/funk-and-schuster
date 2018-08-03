0743535300aab73e00c20755193ae7a86cdc8fc8
Tom Schuster <tomjschuster@gmail.com>
2018-08-03 00:25:00 -0400

    hide thesis unless subdomain is admin

--- lib/funk_and_schuster_web/controllers/page_controller.ex
+++ lib/funk_and_schuster_web/controllers/page_controller.ex
@@ -1,9 +1,18 @@
 defmodule FunkAndSchusterWeb.PageController do
   use FunkAndSchusterWeb, :controller
 
-  def index(conn, _params), do: render(conn, :index)
-  def about(conn, _params), do: render(conn, :about)
-  def gallery(conn, _params), do: render(conn, :gallery)
-  def artists(conn, _params), do: render(conn, :artists)
-  def contact(conn, _params), do: render(conn, :contact)
+  def admin_host?(conn) do
+    case conn.host do
+      "admin.funkandschuster.com" -> true
+      "admin.localhost" -> true
+      _ -> false
+    end
+  end
+
+
+  def index(conn, _params), do: render(conn, :index, thesis_editable: admin_host?(conn))
+  def about(conn, _params), do: render(conn, :about, thesis_editable: admin_host?(conn))
+  def gallery(conn, _params), do: render(conn, :gallery, thesis_editable: admin_host?(conn))
+  def artists(conn, _params), do: render(conn, :artists, thesis_editable: admin_host?(conn))
+  def contact(conn, _params), do: render(conn, :contact, thesis_editable: admin_host?(conn))
 end

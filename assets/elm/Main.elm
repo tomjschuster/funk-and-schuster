5414579030cd6cbba190f60e7bf9e5828ababa30
Tom Schuster <tomjschuster@gmail.com>
2018-12-26 01:23:39 -0500

    elm types/decoders for artist/work/media

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -2,15 +2,167 @@ module Main exposing (main)
 
 import Browser
 import Html
+import Json.Decode as JD
+import Time
+import Url exposing (Url)
 
 
-main : Program () () ()
+main : Program () Model ()
 main =
     Browser.application
-        { init = \() _ _ -> ( (), Cmd.none )
-        , view = \() -> { title = "hello elm", body = [ Html.text "hi there" ] }
-        , update = \() () -> ( (), Cmd.none )
-        , subscriptions = \() -> Sub.none
+        { init = \() _ _ -> ( initialModel, Cmd.none )
+        , view = \model -> { title = "hello elm", body = [ Html.text "hi there?" ] }
+        , update = \() model -> ( initialModel, Cmd.none )
+        , subscriptions = \model -> Sub.none
         , onUrlRequest = \_ -> ()
         , onUrlChange = \_ -> ()
         }
+
+
+type alias Model =
+    { artists : List Artist, works : List Work, media : List Media }
+
+
+initialModel : Model
+initialModel =
+    { artists = [], works = [], media = [] }
+
+
+
+-- Artist
+
+
+type alias Artist =
+    { id : ArtistId
+    , firstName : String
+    , lastName : String
+    , dob : Time.Posix
+    }
+
+
+type ArtistId
+    = ArtistId Int
+
+
+artistDecoder : JD.Decoder Artist
+artistDecoder =
+    JD.map4 Artist
+        (JD.field "id" artistIdDecoder)
+        (JD.field "firstName" JD.string)
+        (JD.field "lastName" JD.string)
+        (JD.field "dob" (JD.map Time.millisToPosix JD.int))
+
+
+artistIdDecoder : JD.Decoder ArtistId
+artistIdDecoder =
+    JD.map ArtistId JD.int
+
+
+
+-- Work
+
+
+type alias Work =
+    { id : WorkId
+    , artistId : ArtistId
+    , title : String
+    , date : Time.Posix
+    , medium : Medium
+    , dimensions : Dimensions
+    }
+
+
+type WorkId
+    = WorkId Int
+
+
+type Medium
+    = OtherMedium String
+
+
+type Dimensions
+    = CustomDimensoins String
+
+
+workDecoder : JD.Decoder Work
+workDecoder =
+    JD.map6 Work
+        (JD.field "id" workIdDecoder)
+        (JD.field "artistId" artistIdDecoder)
+        (JD.field "title" JD.string)
+        (JD.field "date" (JD.map Time.millisToPosix JD.int))
+        (JD.field "medium" mediumDecoder)
+        (JD.field "dimensions" dimensionsDecoder)
+
+
+workIdDecoder : JD.Decoder WorkId
+workIdDecoder =
+    JD.map WorkId JD.int
+
+
+mediumDecoder : JD.Decoder Medium
+mediumDecoder =
+    JD.map OtherMedium JD.string
+
+
+dimensionsDecoder : JD.Decoder Dimensions
+dimensionsDecoder =
+    JD.map CustomDimensoins JD.string
+
+
+
+-- Media
+
+
+type alias Media =
+    { id : MediaId
+    , owner : MediaOwner
+    , title : String
+    , caption : String
+    , src : String
+    , contentType : ContentType
+    }
+
+
+type MediaId
+    = MediaId Int
+
+
+type MediaOwner
+    = ArtistOwner ArtistId
+    | WorkOwner WorkId
+    | NoOwner
+
+
+type ContentType
+    = OtherContentType String
+
+
+mediaDecoder : JD.Decoder Media
+mediaDecoder =
+    JD.map6 Media
+        (JD.field "id" mediaIdDecoder)
+        mediaOwnerDecoder
+        (JD.field "title" JD.string)
+        (JD.field "caption" JD.string)
+        (JD.field "src" JD.string)
+        (JD.field "contentType" contentTypeDecoder)
+
+
+mediaIdDecoder : JD.Decoder MediaId
+mediaIdDecoder =
+    JD.map MediaId JD.int
+
+
+mediaOwnerDecoder : JD.Decoder MediaOwner
+mediaOwnerDecoder =
+    JD.oneOf
+        [ JD.field "artistId" (JD.map ArtistOwner artistIdDecoder)
+        , JD.field "workId" (JD.map WorkOwner workIdDecoder)
+        , JD.succeed NoOwner
+        ]
+
+
+contentTypeDecoder : JD.Decoder ContentType
+contentTypeDecoder =
+    JD.map OtherContentType JD.string

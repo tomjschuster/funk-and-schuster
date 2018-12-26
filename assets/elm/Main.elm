b8d6bd81fc5a0b31d68427ddb48c0431d9028d83
Tom Schuster <tomjschuster@gmail.com>
2018-12-26 00:45:15 -0500

    server elm spa at art rout

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -1,7 +1,16 @@
 module Main exposing (main)
 
+import Browser
 import Html
 
 
+main : Program () () ()
 main =
-    Html.text "hello world"
+    Browser.application
+        { init = \() _ _ -> ( (), Cmd.none )
+        , view = \() -> { title = "hello elm", body = [ Html.text "hi there" ] }
+        , update = \() () -> ( (), Cmd.none )
+        , subscriptions = \() -> Sub.none
+        , onUrlRequest = \_ -> ()
+        , onUrlChange = \_ -> ()
+        }
